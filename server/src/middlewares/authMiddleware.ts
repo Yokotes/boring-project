8a67ce116db023b66908a659ed84074f568b5b89
Yokotes <takjchannel@gmail.com>
2026-07-28 14:21:35 +0300

    feat: add migrations for exercises

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -1,14 +1,18 @@
-import { type Request, type Response } from "express";
+import { type NextFunction, type Request, type Response } from "express";
 import { getTokenFromCookie } from "../utils";
 import { checkAuth } from "../services";
 
-export const authMiddleware = async (req: Request, res: Response) => {
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   const token = getTokenFromCookie(req.headers["cookie"]);
 
   if (token) {
     const user = await checkAuth(token);
 
-    if (user) return;
+    if (user) return next();
   }
 
   res.sendStatus(401);
