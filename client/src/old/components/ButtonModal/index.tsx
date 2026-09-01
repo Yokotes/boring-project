930e4e0bbd2224a45ac545ed65565398c326406a
Yokotes <takjchannel@gmail.com>
2026-09-01 15:06:40 +0300

    remove: old package

--- client/src/old/components/ButtonModal/index.tsx
+++
@@ -1,2 +0,0 @@
-// TODO: Find out why is eslint showing warning
-export * from "./ButtonModal";
--- client/src/old/components/TextArea/TextArea.tsx
+++
@@ -1,6 +0,0 @@
-import type { ComponentPropsWithRef, FC } from "react";
-import styles from "./TextArea.module.scss";
-
-export const TextArea: FC<ComponentPropsWithRef<"textarea">> = (props) => {
-  return <textarea className={styles.textarea} {...props} />;
-};
